48c074c166c5066769ecb0d83387458bbdbb6633
Isaque João <isaqueijs@hotmail.com>
2020-01-14 23:09:24 -0300

    primeiro commmit

---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://isaque:08008908211@cluster0-tmit5.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+app.use(express.json());
+app.use(routes);
+
+// Métodos HTTP: GET, POST, PUT, DELETE
+
+// Tipos de parâmetros
+
+// Query Params: request.query (Filtros, ordenação, paginação, ...)
+// Route Params: request.params (identificar um recurso na alteração ou remoção)
+// Body: request.body (Dados para criação ou alteração de um registro)
+
+app.listen(3333);
