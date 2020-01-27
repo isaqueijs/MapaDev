228dbd525df82198a722c8d4838914a68c4856e2
unknown <suporte@STFRECC031789-W.stefanini.dom>
2020-01-27 14:43:37 -0300

    Terminando a aplicação web

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -9,6 +10,7 @@ mongoose.connect('mongodb+srv://isaque:08008908211@cluster0-tmit5.mongodb.net/we
     useUnifiedTopology: true,
 });
 
+app.use(cors())
 app.use(express.json());
 app.use(routes);
 
